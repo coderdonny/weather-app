3e30d4b30b8cf8f32e91eb6468814dcdd90ba60e
Daniel Karpierz <dankarp1999@gmail.com>
2022-12-03 17:13:43 -0500

    Create data handler function

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 const body = document.querySelector('body');
 async function getWeather() {
-	const response = await fetch(
-		`https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=0ba7cce150f486819412ea0336b94a65
-        `,
-		{ mode: 'cors' }
-	);
-	const weatherData = await response.json();
+	try {
+		const response = await fetch(
+			`https://api.openweathermap.org/data/2.5/weather?lat=43.58&lon=79.64&appid=0ba7cce150f486819412ea0336b94a65
+			`,
+			{ mode: 'cors' }
+		);
+		const weatherData = await response.json();
 
-	console.log(weatherData);
+		return weatherData;
+	} catch (error) {
+		console.log(error);
+	}
+}
 
+function handleData(weatherData) {
 	const feelsLike = document.createElement('h1');
 	const temp = document.createElement('h1');
 	const weather = document.createElement('h1');
@@ -18,8 +24,9 @@ async function getWeather() {
 	body.appendChild(feelsLike);
 	body.appendChild(temp);
 	body.appendChild(weather);
-
-	return weatherData;
 }
 
-let weather = getWeather();
+getWeather().then((data) => {
+	console.log(data);
+	handleData(data);
+});
