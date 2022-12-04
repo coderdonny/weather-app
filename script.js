e9b71687fe3dee2d071d734bf1a19a764b88ab21
Daniel Karpierz <dankarp1999@gmail.com>
2022-12-04 14:18:10 -0500

    Implement geocoding API to get coordinates from location name

--- script.js
+++ script.js
@@ -1,11 +1,43 @@
 const body = document.querySelector('body');
-async function getWeather() {
+
+async function getLocation() {
 	try {
 		const response = await fetch(
-			`https://api.openweathermap.org/data/2.5/weather?lat=43.58&lon=79.64&appid=0ba7cce150f486819412ea0336b94a65
+			`http://api.openweathermap.org/geo/1.0/direct?q=Toronto&appid=0ba7cce150f486819412ea0336b94a65
 			`,
 			{ mode: 'cors' }
 		);
+		const locationData = await response.json();
+
+		console.log('latitude:' + ' ' + locationData[0].lat);
+		console.log('longitude:' + ' ' + locationData[0].lon);
+
+		return locationData;
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+getLocation().then((data) => {
+	getCoordinates(data);
+});
+
+function getCoordinates(data) {
+	let lat = data[0].lat;
+	let lon = data[0].lon;
+
+	getWeather(lat, lon).then((data) => {
+		console.log(data.weather[0].main);
+		console.log(data);
+		handleData(data);
+	});
+}
+
+async function getWeather(lat, lon) {
+	try {
+		let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=0ba7cce150f486819412ea0336b94a65`;
+
+		const response = await fetch(url, { mode: 'cors' });
 		const weatherData = await response.json();
 
 		return weatherData;
@@ -15,18 +47,7 @@ async function getWeather() {
 }
 
 function handleData(weatherData) {
-	const feelsLike = document.createElement('h1');
-	const temp = document.createElement('h1');
-	const weather = document.createElement('h1');
-	feelsLike.innerText = weatherData.main.feels_like;
-	temp.innerText = weatherData.main.temp;
-	weather.innerText = weatherData.wind.deg;
-	body.appendChild(feelsLike);
-	body.appendChild(temp);
-	body.appendChild(weather);
+	let feelsLike = weatherData.main.feels_like;
+	let temp = weatherData.main.temp;
+	let wind = weatherData.wind.deg;
 }
-
-getWeather().then((data) => {
-	console.log(data);
-	handleData(data);
-});
