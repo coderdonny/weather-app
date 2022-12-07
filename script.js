336509796e5f4cb2ed2bdacc9443e75ae4375331
Daniel Karpierz <dankarp1999@gmail.com>
2022-12-07 18:25:06 -0500

    Add some styling and pass location data through to displayData function

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const body = document.querySelector('body');
+const weatherContainer = document.querySelector('#weather-container');
 const search = document.querySelector('#location');
 const searchForm = document.querySelector('.search-form');
+const unitsOfMeasurement = document.querySelector('#units');
 
 searchForm.addEventListener('submit', function (e) {
 	e.preventDefault();
@@ -16,17 +17,21 @@ async function getLocation(location) {
 		const response = await fetch(url, { mode: 'cors' });
 		const locationData = await response.json();
 
+		console.log(locationData);
+		console.log(locationData[0].country);
+		console.log(locationData[0].state);
+		console.log(locationData[0].name);
+
 		console.log('latitude:' + ' ' + locationData[0].lat);
 		console.log('longitude:' + ' ' + locationData[0].lon);
 
-		getWeather(locationData[0].lat, locationData[0].lon);
+		getWeather(locationData[0].lat, locationData[0].lon, locationData);
 	} catch (error) {
 		console.log(error);
 	}
 }
 
-async function getWeather(lat, lon) {
-	const unitsOfMeasurement = document.querySelector('#units');
+async function getWeather(lat, lon, locationData) {
 	const unit =
 		unitsOfMeasurement.options[unitsOfMeasurement.selectedIndex].value;
 	try {
@@ -35,34 +40,52 @@ async function getWeather(lat, lon) {
 		const response = await fetch(url, { mode: 'cors' });
 		const weatherData = await response.json();
 
-		handleData(weatherData);
+		handleData(weatherData, locationData);
 	} catch (error) {
 		console.log(error);
 	}
 }
 
-function handleData(weatherData) {
+function handleData(weatherData, locationData) {
+	let state = locationData[0].state;
+	let city = locationData[0].name;
 	let weather = weatherData.weather[0].main;
 	let feelsLike = weatherData.main.feels_like;
 	let temp = weatherData.main.temp;
 	let wind = weatherData.wind.deg;
 
-	let weatherDataObj = {
+	const outputDataObj = {
+		state: state,
+		city: city,
 		weather: weather,
 		feelsLike: feelsLike,
 		temperature: temp,
 		wind: wind,
 	};
 
-	console.log(weatherDataObj);
-	displayData(weatherDataObj);
+	console.log(outputDataObj);
+	displayData(outputDataObj);
 }
 
 function displayData(data) {
+	const location = document.createElement('h1');
+	location.className = 'location-information';
+	location.innerText = `${data.city}, ${data.state}`;
+
 	const weather = document.createElement('h1');
 	weather.innerText = data.weather;
+
 	const temp = document.createElement('h1');
-	temp.innerText = data.temperature;
-	body.append(weather);
-	body.append(temp);
+
+	let units =
+		unitsOfMeasurement.options[unitsOfMeasurement.selectedIndex].value;
+	if (units === 'metric') {
+		temp.innerText = temp.innerText = data.temperature + '°' + ' ' + 'C';
+	} else if (units === 'imperial') {
+		temp.innerText = temp.innerText = data.temperature + '°' + ' ' + 'F';
+	}
+
+	weatherContainer.append(location);
+	weatherContainer.append(temp);
+	weatherContainer.append(weather);
 }
